612b2e28bfe99b4d4e1a14a73b4b183069ad8fef
Rachel Perez <reperez@me.com>
2019-09-05 12:19:22 -0400

    new commit

--- src/config.js
+++ src/config.js
@@ -2,7 +2,9 @@ module.exports = {
     PORT: process.env.PORT || 8000,
     NODE_ENV: process.env.NODE_ENV || 'development',
     DB_URL: process.env.DATABASE_URL || 'postgresql://rachelemilyperez@localhost/noteful',
-    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://rocky-gorge-29682.herokuapp.com/'
-
+    API_BASE_URL: process.env.REACT_APP_API_BASE_URL || 'https://noteful-app.reperez120.now.sh/'
 }
+
+
+    // 'https://rocky-gorge-29682.herokuapp.com/'
   
